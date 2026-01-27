2e2af5c03d1c2295d1f66ce4aa8b15a2ad82d03f
Mikhail Bazarsadaev <mbazarsadaev@yandex.ru>
2026-01-27 13:42:57 +0000

    style fix

--- src/robotics/main.py
+++ src/robotics/main.py
@@ -2,7 +2,8 @@ import math
 import time
 from statistics import mean
 from typing import List
-import brickpi3  # type: ignore
+
+import brickpi3
 
 from robotics.utils import Rotation, WheelMovement, allF, forEach
 
@@ -13,8 +14,8 @@ LEFT_WHEEL = BP.PORT_A
 RIGHT_WHEEL = BP.PORT_B
 
 # Other constants
-POLLING_INTERVAL = 0.2  # seconds
-POWER_LIMIT = 70  # value between 0 to 100
+POLLING_INTERVAL = 0.2 # seconds
+POWER_LIMIT = 70 # value between 0 to 100
 
 # Units in cm unless stated otherwise
 wheel_radius = 3.0
@@ -22,10 +23,8 @@ wheel_separation = 16.0
 
 # !! Measured data !!
 # Please calibrate these before using them using the appropriate calibration functions
-max_dps = 150.0  # maximum degrees per second
-# represents the multiplier between motor rotation and distance moved
-radius_modifier = 1.0
-
+max_dps = 150.0 # maximum degrees per second
+radius_modifier = 1.0 # represents the multiplier between motor rotation and distance moved
 
 def calibrate_max_dps():
     """
@@ -38,7 +37,7 @@ def calibrate_max_dps():
         BP.set_motor_limits(wheel, POWER_LIMIT)
         BP.set_motor_power(wheel, POWER_LIMIT)
 
-    time.sleep(3)  # To reach max speed
+    time.sleep(3) # To reach max speed
 
     for wheel in [LEFT_WHEEL, RIGHT_WHEEL]:
         BP.reset_motor_encoder(wheel)
@@ -49,9 +48,7 @@ def calibrate_max_dps():
         dps.append(abs(BP.get_motor_encoder(wheel)) * 2)
 
     max_dps = mean(dps)
-    print(
-        f"Calibration for max degrees per second complete\nMax dps = {max_dps}")
-
+    print(f"Calibration for max degrees per second complete\nMax dps = {max_dps}")
 
 def calibrate_radius_modifier(meters: float = 1.0):
     """
@@ -59,10 +56,8 @@ def calibrate_radius_modifier(meters: float = 1.0):
     """
     global radius_modifier
     distance = meters * 100.0
-    motorMovementHandler(list(map(lambda w: WheelMovement(
-        w, distance=distance, speed=max_dps * actual_radius()), both_wheels)))
-    actual_distance = float(
-        input("What is the actual distance traveled (cm)? "))
+    motorMovementHandler(list(map(lambda w : WheelMovement(w, distance = distance, speed =max_dps * actual_radius()), (LEFT_WHEEL, RIGHT_WHEEL))))
+    actual_distance = float(input("What is the actual distance traveled (cm)? "))
     radius_modifier = actual_distance / distance
     print(f"Actual wheel radius: {actual_radius()}")
 
@@ -104,10 +99,10 @@ def motorMovementHandler(movements: List[WheelMovement]):
     forEach(movements, lambda mvmt: mvmt.begin())
 
     # Check every POLLING_INTERVAL the degrees moved by the motors, and subtract that from total degrees required to perform the action
-    while not allF(movements, lambda mvmt: mvmt.is_complete()):
-        forEach(movements, lambda mvmt: mvmt.reset_angle())
+    while not allF(movements, lambda mvmt : mvmt.is_complete()):
+        forEach(movements, lambda mvmt : mvmt.reset_angle())
         time.sleep(POLLING_INTERVAL)
-        forEach(movements, lambda mvmt: mvmt.update())
+        forEach(movements, lambda mvmt : mvmt.update())
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -120,13 +115,8 @@ def forward(distance: float):
     Commands the robot to move forward by this distance in centimeters
     """
     return motorMovementHandler([
-        << << << < HEAD
-        WheelMovement(left_wheel, distance=distance, speed=dps_to_speed()),
-        WheelMovement(right_wheel, distance=distance, speed=dps_to_speed()),
-        == == == =
-        WheelMovement(LEFT_WHEEL, distance=distance, speed=dps_to_speed()),
-        WheelMovement(RIGHT_WHEEL, distance=distance, speed=dps_to_speed()),
-        >>>>>> > 24b8b2e2f90c04b4360b5e59954368d6106b4170
+        WheelMovement(LEFT_WHEEL, distance = distance, speed = dps_to_speed()),
+        WheelMovement(RIGHT_WHEEL, distance = distance, speed = dps_to_speed()),
     ])
 
 
@@ -134,14 +124,11 @@ def turn(direction: Rotation, degrees: float, rotational_speed_modifier: float =
     """
     Turns in the direction, by alpha degrees. The speed of the wheels during the rotation is rotational_speed_modifier * max_dps
     """
-    forward_wheel, backward_wheel = (
-        LEFT_WHEEL, RIGHT_WHEEL) if direction == Rotation.Clockwise else (LEFT_WHEEL, RIGHT_WHEEL)
+    forward_wheel, backward_wheel = (LEFT_WHEEL, RIGHT_WHEEL) if direction == Rotation.Clockwise else (LEFT_WHEEL, RIGHT_WHEEL)
     distance = wheel_separation * rad(degrees)
     motorMovementHandler([
-        WheelMovement(forward_wheel, distance=distance,
-                      speed=dps_to_speed(reduction_factor=0.5)),
-        WheelMovement(backward_wheel, distance=-distance,
-                      speed=dps_to_speed(reduction_factor=0.5))
+        WheelMovement(forward_wheel, distance = distance, speed = dps_to_speed(reduction_factor=0.5)),
+        WheelMovement(backward_wheel, distance = -distance, speed = dps_to_speed(reduction_factor=0.5))
     ])
 
 
